dbd193166dd82e28496e70db2a9113235e4f27b9
Vitalik <Wital2010@gmail.com>
2020-04-23 00:14:06 +0300

    Пушу версию без консоль логов

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('cssnano')({
-            preset: 'default',
-        })
-    ]
-}
-// 65 еслпи писать browser:last 10 version
